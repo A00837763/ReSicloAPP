e18409763f2addcb7d99c15195b0697cd22720f6
Santiagoschzr <a00837763@tec.mx>
2024-11-02 12:30:48 -0600

    Commented Maker Struct For enhanced Understanding

--- ResicloApp/Map/Marker/Marker.swift
+++ ResicloApp/Map/Marker/Marker.swift
@@ -1,19 +1,21 @@
 import SwiftUI
 import MapKit
 
+//Estructura de Ubicación de Centro de Reciclaje
 struct CollectionMarker: Identifiable, Codable, Equatable {
-    let id: Int
-    let name: String
-    let lng: Double
-    let lat: Double
-    let address: String
+    let id: Int //id único del centro
+    let name: String //Nombre del Centro
+    let lng: Double //Longitud
+    let lat: Double // Latitud
+    let address: String // Dirección
     
-    var coordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: lat , longitude: lng)
+    // Conversión valores de Longitud y Latitud a Coordenadas en 2d En el Espacio
+    var coordinate: CLLocationCoordinate2D { //Variable llamada coordinate de tipo CLlocationCoordinate2D
+        CLLocationCoordinate2D(latitude: lat , longitude: lng) // Constructor de la coordenada
     }
     
     enum CodingKeys: String, CodingKey {
-        case id, name, lng, lat
-        case address = "address_text"
+        case id, name, lng, lat //Se llaman igual en la API los atributos
+        case address = "address_text" // En la api se llama address_text
     }
 }
